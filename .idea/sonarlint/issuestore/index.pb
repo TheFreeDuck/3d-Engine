d7aaa4ebd750c6082f69924997392502f94db13d
TheFreeDuck <fredrikthorcewers@gmail.com>
2022-10-20 10:40:10 +0200

    update

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+F
+.idea/libraries/3D.xml,3\6\364e5d7e467731aac08954b2df20de3fc6c780fc
+M
+.idea/libraries/3d_Engine.xml,5\3\5397ce1bfa69b8f3f0d35375c39effbff5d7939f
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+Q
+!.idea/artifacts/3d_Engine_jar.xml,3\c\3cfb459eb73f281787802ab3df72ade691817e47
+=
+3d-Engine.iml,3\9\398630acce88dde86141a2d933e1205d9eb25ef9
+R
+"Java Project/3D/src/main/Main.java,c\b\cb6435f7c47222eeb0e60d1d0b3c1b34d5a85783
---
+++ Java Project/3DEngine/3D/src/math/Ray.java
@@ -0,0 +1,42 @@
+package math;
+
+/**
+ *
+ * @author Fredrik
+ */
+public class Ray {
+
+    private Point3d p1;
+    private Point3d p2;
+
+    public Ray(Point3d p1, Point3d p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Vector getVector() {
+        return new Vector(p2.getX() - p1.getX(), p2.getY() - p1.getY(), p2.getZ() - p1.getZ());
+    }
+
+    public double length() {
+        return this.getVector().scalar();
+    }
+
+    public Point3d getP1() {
+        return p1;
+    }
+
+    public void setP1(Point3d p1) {
+        this.p1 = p1;
+    }
+
+    public Point3d getP2() {
+        return p2;
+    }
+
+    public void setP2(Point3d p2) {
+        this.p2 = p2;
+    }
+    
+    
+}
