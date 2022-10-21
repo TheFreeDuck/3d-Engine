8c29a3ac26814ca7b806ef1d3a86d40264510809
TheFreeDuck <fredrikthorcewers@gmail.com>
2022-10-21 10:09:43 +0200

    Update

--- Java Projects/Launcher/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-K
-Launcher/src/main/main.java,a\b\ab2c3affe2a6a8b6a42fada80e78d341ccc81e7c
-L
-Launcher/src/main/Frame.java,4\a\4ad89264aca23e5c31df4324e360f9cac2246496
-L
-Launcher/src/main/Frame.form,9\a\9abd2c08f900d8b553c2bf243a1734fecc475df6
--- Java Projects/Launcher/Launcher/src/main/Frame.java
+++ Java Projects/Launcher/Launcher/src/main/Frame.java
@@ -1,10 +1,18 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class Frame extends JFrame {
     private JPanel panel;
-    private JButton button1;
+    private JButton downloadAndRunButton;
 
     public Frame(){
         setContentPane(panel);
@@ -14,5 +22,19 @@ public class Frame extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 
+        downloadAndRunButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    URL url = new URL("https://fabulous-puffpuff-8f0c55.netlify.app/resources/jar/3d-Engine.jar");
+                    ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+                    FileOutputStream fos = new FileOutputStream(System.getProperty("user.home") + "/Downloads/downloaded.jar");
+                    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                    Desktop.getDesktop().open(new File(System.getProperty("user.home") + "/Downloads/downloaded.jar"));
+                }catch(Exception er){
+                    System.out.println(er);
+                }
+            }
+        });
     }
 }
