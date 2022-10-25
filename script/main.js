db282486d52178b1b21197abba102ab8d2b53991
Clubboomers <tim@hogberg.net>
2022-10-25 23:43:30 +0200

    quality of life changes

--- script/main.js
+++ script/main.js
@@ -9,28 +9,13 @@ $("#switch").change(function() {
         $("img").on("hover", () => {
             alert("Virus detected!! Download free nordic vpnfor delete");
         });
-        function colorChange() {
-            let r = Math.floor(Math.random()*256);
-            let g = Math.floor(Math.random()*256);
-            let b = Math.floor(Math.random()*256);
-            let color = rgbToHex(r,g,b);
-            $("body").css("background-color", color);
-            }
         $("body").on("mousemove", colorChange);
-        function componentToHex(c) {
-            var hex = c.toString(16);
-            return hex.length == 1 ? "0" + hex : hex;
-        }
-        
-        function rgbToHex(r, g, b) {
-            return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-        }
         $("img").animate({width: '400'}, "slow");
         $("img").animate({width: '200'}, "slow");
         animateInterval = setInterval(() => {
             $("img").animate({width: '400'}, "slow");
             $("img").animate({width: '200'}, "slow");
-        }, 1000);
+        }, 1200);
     } else if ($('#switch').prop('checked') == false) {
         //$("html").remove();
         clearInterval(animateInterval);
@@ -41,3 +26,20 @@ $("#switch").change(function() {
         $("body").css("background-color", "#181818");
     }
 });
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function colorChange() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    let color = rgbToHex(r,g,b);
+    $("body").css("background-color", color);
+}
